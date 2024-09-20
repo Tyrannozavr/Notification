6805c0b0d256339565c7c4a2a8949d74e7eea753
dmiv <Dmitriy40647274@gmail.com>
2024-09-21 02:05:42 +0300

    switch double enter a password

--- telegram/authentication/routes.py
+++ telegram/authentication/routes.py
@@ -12,16 +12,23 @@ def register_authentication_handlers(dp):
     async def set_username(message: Message, state: FSMContext):
         username = message.text
         await state.update_data(username=username)  # Store username in state
-        await message.answer(f'Здравствуйте {username}, введите пароль:')
-        # await Registration.password.set()  # Move to password state
-        await state.set_state(Registration.password)
+        user_data = await state.get_data()
+        if user_data.get('password') is None:
+            await message.answer(f'Здравствуйте {username}, введите пароль:')
+            # await Registration.password.set()  # Move to password state
+            await state.set_state(Registration.password)
+        else:
+            await set_password(message=message, state=state)
 
     @dp.message(Registration.password)
     async def set_password(message: Message, state: FSMContext):
-        password = message.text
         user_data = await state.get_data()  # Retrieve user data from state
         username = user_data.get('username')  # Get the stored username
 
+        password = user_data.get('password')
+        if password is None:
+            password = message.text
+            await state.update_data(password=password)
         response = register_account(username=username, password=password)
 
         if response.status_code == 201:
